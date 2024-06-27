4bf7258ce37aca54bde6cfd025c37a17554ed4f9
Vasil Kostadinov <v.kostadinov@dreamshot.bg>
2024-06-27 14:39:04 +0300

    fix: change vite output config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "",
   plugins: [react()],
+  build: {
+    outDir: "build/",
+    rollupOptions: {
+      output: {
+        entryFileNames: "static/js/main.js",
+        assetFileNames: "static/css/main.css",
+      },
+    },
+  },
 });
